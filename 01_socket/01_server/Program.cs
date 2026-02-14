dfbc9ff9e2a40a7b974c0ed6c2fe95c26765de66
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-14 22:36:29 +0300

    add: socket

---
+++ 01_socket/01_server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+// const string serverIp = "127.0.0.1";
+const string serverIp = "192.168.2.150";
+const int port = 80;
+
+// Создание TCP/IPv4 сокета
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+
+try
+{
+	socket.Bind(endpoint);
+	socket.Listen();
+
+    Console.WriteLine($"Server started at {serverIp}:{port}");
+
+	while(true)
+	{
+
+        // BLOCKING (до тех пор, пока не установиться успекшное соединение)
+        // Ожидание нового подключения
+        // 1. Абстакция над установленным соединением с конкретным клиентом
+        // 2. Реальный системный сокет для общения с конкретным клиентом
+        Socket remoteSocket = socket.Accept();
+
+        Console.WriteLine("Connection established...");
+
+        byte[] buffer = new byte[1024];
+        int bytesCount = 0;
+        StringBuilder sb = new StringBuilder();
+        do
+        {
+            bytesCount = remoteSocket.Receive(buffer);
+            sb.Append(Encoding.UTF8.GetString(buffer, 0, bytesCount));
+        } while (remoteSocket.Available > 0);
+
+        Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {sb.ToString()}");
+
+        Thread.Sleep(2000);
+
+        string response = "Hello from server";
+//        string response = @"HTTP/1.1 200 OK
+//Content-Type: text/html; charset=utf-8
+//Connection: close
+
+//<h1 style='color:red;'>Vasia</h1>";
+
+        remoteSocket.Send(Encoding.UTF8.GetBytes(response));
+
+
+        // Закрытие соединения и удаление remoteSocket
+        remoteSocket.Shutdown(SocketShutdown.Both);     // Выполнение протокола закрытия соединения
+        remoteSocket.Close();                           // Освобождение ресурсов из-под remoteSocket
+
+        Console.WriteLine("Connection closed...");
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+
+
+
+
---
+++ 01_socket/02_client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// const string serverIp = "127.0.0.1";
+const string serverIp = "192.168.2.150";
+const int serverPort = 80;
+
+// Создание TCP/IPv4 сокета
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+try
+{
+    Console.Write("> ");
+    string? message = Console.ReadLine();
+
+    if (message is null)
+        throw new ArgumentException("Message is empty");
+
+    socket.Connect(serverEndpoint);                 // BLOCKING
+    socket.Send(Encoding.UTF8.GetBytes(message));   // BLOCKING
+
+    byte[] buffer = new byte[1024];
+    int bytesCount = 0;
+    StringBuilder sb = new StringBuilder();
+    do
+    {
+        bytesCount = socket.Receive(buffer);        // BLOCKING
+        sb.Append(Encoding.UTF8.GetString(buffer, 0, bytesCount));
+    } while (socket.Available > 0);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: Response: {sb.ToString()}");
+
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+
+    Console.WriteLine("Connection closed");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
